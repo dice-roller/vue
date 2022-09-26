d2f9ea65bb3ae276b33dace5608f9d0e5728c12a
Lee Langley <lee@greenimp.co.uk>
2022-09-26 21:42:13 +0100

    Better check for `crypto`

--- src/composables/idGenerator.js
+++ src/composables/idGenerator.js
@@ -1,6 +1,14 @@
 import { ref } from 'vue';
 
-const getRandomId = () => crypto ? crypto.randomUUID() : `${Date.now().toString(32)}-${Math.random().toString(36).substring(2)}`;
+const hasCrypto = () => typeof crypto !== 'undefined' && crypto && crypto.randomUUID;
+
+const getRandomId = () => {
+  if (hasCrypto){
+    return crypto.randomUUID();
+  }
+
+  return `${Date.now().toString(32)}-${Math.random().toString(36).substring(2)}`;
+};
 
 export function useIdGenerator(prefix) {
   const id = ref(null);
